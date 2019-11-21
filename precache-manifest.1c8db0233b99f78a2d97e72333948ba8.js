ad3a8b19d1c64ee79a0a45bf4b4752895b5f8997
zerosoul <yanggc888@163.com>
2019-11-21 17:50:02 +0800

    Updates

---
+++ precache-manifest.1c8db0233b99f78a2d97e72333948ba8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35674835c268742a4a4d0e52d0f5d38b",
+    "url": "/rc-bullets/index.html"
+  },
+  {
+    "revision": "b6dba5930dc4c42e35f0",
+    "url": "/rc-bullets/static/js/2.1db03b8b.chunk.js"
+  },
+  {
+    "revision": "c80d55c4b7bbd60aed2a",
+    "url": "/rc-bullets/static/js/main.47677026.chunk.js"
+  },
+  {
+    "revision": "7b11d386419b4468299c",
+    "url": "/rc-bullets/static/js/runtime-main.0cd9117e.js"
+  },
+  {
+    "revision": "838852664a4ae302f8426890eff5217b",
+    "url": "/rc-bullets/static/media/avator.83885266.jpg"
+  }
+]);
