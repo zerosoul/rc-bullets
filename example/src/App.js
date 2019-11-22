1dd5bb8856eebf847e501f66e094baccd3fca92e
Tristan <yanggc888@163.com>
2019-11-22 22:20:19 +0800

    updates

--- example/src/App.js
+++ example/src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import DemoPage from './containers/Screen';
 import DashboardPage from './containers/Dashboard';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 const App = () => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          primary: {
+            main: '#177cb0'
+          },
+          secondary: { main: '#4c8dae' },
+          error: {
+            main: '#dc3023'
+          },
+          type: prefersDarkMode ? 'dark' : 'light'
+        }
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route path={`/`} exact component={DashboardPage} />
-        <Route path={`/preview`} component={DemoPage} />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route path={`/`} exact component={DashboardPage} />
+          <Route path={`/preview`} component={DemoPage} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 };
 export default App;
--- example/src/containers/Screen.js
+++ example/src/containers/Screen.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
+import { IconButton } from '@material-ui/core';
+import RotateLeftIcon from '@material-ui/icons/RotateLeft';
+import PlayCircleFilledWhiteIcon from '@material-ui/icons/PlayCircleFilledWhite';
+import PauseCircleFilledIcon from '@material-ui/icons/PauseCircleFilled';
+import VisibilityIcon from '@material-ui/icons/Visibility';
 
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 export default function ScreenPage({ screen }) {
   const [paused, setPaused] = useState(false);
   const [visible, setVisible] = useState(true);
@@ -26,9 +32,17 @@ export default function ScreenPage({ screen }) {
 
   return (
     <>
-      <button onClick={handleAnimateState}>{paused ? '继续' : '暂停'}</button>
-      <button onClick={handleClear}>清除</button>
-      <button onClick={handleVisible}>{visible ? '隐藏' : '显示'}</button>
+      <aside style={{ position: 'fixed', left: 0, top: 0, zIndex: 999 }}>
+        <IconButton onClick={handleClear}>
+          <RotateLeftIcon color="secondary">ddd</RotateLeftIcon>
+        </IconButton>
+        <IconButton onClick={handleVisible}>
+          {visible ? <VisibilityOffIcon /> : <VisibilityIcon />}
+        </IconButton>
+        <IconButton onClick={handleAnimateState}>
+          {paused ? <PlayCircleFilledWhiteIcon /> : <PauseCircleFilledIcon />}
+        </IconButton>
+      </aside>
       <section style={{ height: '100vh' }} className="screen"></section>
     </>
   );
