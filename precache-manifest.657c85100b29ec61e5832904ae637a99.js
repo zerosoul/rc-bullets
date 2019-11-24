d91468a14724545f2f83cd78838f3bce61a119c8
Tristan <yanggc888@163.com>
2019-11-24 11:19:26 +0800

    Updates

---
+++ precache-manifest.657c85100b29ec61e5832904ae637a99.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208219d67b8d8682d516db417ad9a661",
+    "url": "/rc-bullets/index.html"
+  },
+  {
+    "revision": "b89e831792cb4a80cb32",
+    "url": "/rc-bullets/static/js/2.77ef7c7f.chunk.js"
+  },
+  {
+    "revision": "60ca0417e13a3768eb4c",
+    "url": "/rc-bullets/static/js/main.468bb30a.chunk.js"
+  },
+  {
+    "revision": "7b11d386419b4468299c",
+    "url": "/rc-bullets/static/js/runtime-main.0cd9117e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rc-bullets/precache-manifest.482bffeb514251b22f98aee8b14d7bff.js"
+  "/rc-bullets/precache-manifest.657c85100b29ec61e5832904ae637a99.js"
 );
 
 self.addEventListener('message', (event) => {
