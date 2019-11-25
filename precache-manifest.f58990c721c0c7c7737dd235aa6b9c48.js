79a2892c5c32b09d30bbf889c46c3a8c2f071a91
zerosoul <yanggc888@163.com>
2019-11-25 14:59:28 +0800

    Updates

---
+++ precache-manifest.f58990c721c0c7c7737dd235aa6b9c48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7942e22f136cfe4f97a42f4684aa574b",
+    "url": "/rc-bullets/index.html"
+  },
+  {
+    "revision": "391b70e5eb422b804293",
+    "url": "/rc-bullets/static/js/2.753e2c46.chunk.js"
+  },
+  {
+    "revision": "9652715b819d26dcc09aad3261a56a36",
+    "url": "/rc-bullets/static/js/2.753e2c46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "642791220217360e0f8e",
+    "url": "/rc-bullets/static/js/main.5d91d83d.chunk.js"
+  },
+  {
+    "revision": "6c2366a422341768c3ad",
+    "url": "/rc-bullets/static/js/runtime-main.437ac50e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rc-bullets/precache-manifest.8aa9c4beeddd381b25c08779872f2a1c.js"
+  "/rc-bullets/precache-manifest.f58990c721c0c7c7737dd235aa6b9c48.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rc-bullets/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
